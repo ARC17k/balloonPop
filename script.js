70fb1f4779f618101dd45f28125d483e0f89bdf1
ARC17k <90900878+ARC17k@users.noreply.github.com>
2024-08-15 00:38:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,154 +1,106 @@
-const balloonImages = [
-    'Symbol100001.png',
-    'Symbol100002.png',
-    'Symbol100003.png',
-    'Symbol100004.png',
-    'Symbol100005.png',
-    'Symbol100006.png',
-    'Symbol100007.png',
-    'Symbol100008.png',
-    'Symbol100009.png',
-    'Symbol100010.png'
-];
+@import url('https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
 
-const alphabets = [
-    'Symbol10001.png',
-    'Symbol10002.png',
-    'Symbol10003.png',
-    'Symbol10004.png',
-    'Symbol10005.png',
-    'Symbol10006.png',
-    'Symbol10007.png',
-    'Symbol10008.png',
-    'Symbol10009.png',
-    'Symbol10010.png',
-    'Symbol10011.png',
-    'Symbol10012.png',
-    'Symbol10013.png',
-    'Symbol10014.png',
-    'Symbol10015.png',
-    'Symbol10016.png',
-    'Symbol10017.png',
-    'Symbol10018.png',
-    'Symbol10019.png',
-    'Symbol10020.png',
-    'Symbol10021.png',
-    'Symbol10022.png',
-    'Symbol10023.png',
-    'Symbol10024.png',
-    'Symbol10025.png',
-    'Symbol10026.png'
-];
-
-let i = 0;
-
-// Function to preload images
-function preloadImages(imageArray, folder) {
-    const preloadedImages = [];
-    imageArray.forEach(image => {
-        const img = new Image();
-        img.src = `assets/${folder}/${image}`;
-        preloadedImages.push(img);
-    });
-    return preloadedImages;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box ;
+    cursor: none;
 }
 
-// Preload balloon and alphabet images
-const preloadedBalloonImages = preloadImages(balloonImages, 'balloonsImg');
-const preloadedAlphabetImages = preloadImages(alphabets, 'alphabets');
+body{
+    background-color: lightblue;
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
 
-
-function setupBalloon(balloon, x, y) {
-    // Randomly select an image for each balloon
-    const randomIndex = Math.floor(Math.random() * balloonImages.length);
-    const randomImage = balloonImages[randomIndex];
-    
-    const randomAIndex = Math.floor(Math.random() * alphabets.length);
-    const randomAImage = alphabets[randomAIndex];
-
-    // Set the background image of the balloon
-    balloon.style.backgroundImage = `url('assets/balloonsImg/${randomImage}')`;
-    balloon.style.backgroundSize = 'cover'; // Optional: Ensure the image covers the entire balloon
-
-    balloon.style.setProperty('--alpha-image', `url('assets/alphabets/${randomAImage}')`);
-    balloon.style.setProperty('--alpha-size', 'contain');
-
-    let posX = x;
-    let posY = y;
-    let yBuffer = Math.random();
-    let xBuffer = Math.random();
-    let velocityX = 2 * xBuffer; // Horizontal speed (pixels per frame)
-    let velocityY = 1 * yBuffer;
-    let animationFrameId; // Declare animationFrameId here
-
-    setInterval(() => {
-        yBuffer = Math.random() * 10;
-        xBuffer = Math.random() * 10;
-    }, 3000);
-
-    function moveBalloon() {
-        const balloonWidth = balloon.offsetWidth;
-        const balloonHeight = balloon.offsetHeight;
-        const viewportWidth = window.innerWidth;
-        const viewportHeight = window.innerHeight;
-
-        // Update position
-        posX += velocityX;
-        posY -= velocityY;
-
-        // Check collision with left or right walls
-        if (posX + balloonWidth > viewportWidth || posX < 0) {
-            velocityX = -velocityX; // Reverse direction on X axis
-            if (velocityY < 0) {
-                velocityY = -velocityY * Math.random() * 2;
-            }
-        }
-
-        // Check collision with top or bottom walls
-        if (posY + balloonHeight > viewportHeight || posY < 0) {
-            velocityY = -velocityY;
-        }
-
-        // Apply new position
-        balloon.style.left = posX + 'px';
-        balloon.style.top = posY + 'px';
-
-        // Save the animation frame ID so it can be canceled later
-        animationFrameId = requestAnimationFrame(moveBalloon);
-    }
-
-    // Start the animation for this balloon
-    moveBalloon();
-
-    balloon.addEventListener("click", (event) => {
-        balloon.style.transition = ".5s ease";
-        balloon.style.opacity = 0;
-        balloon.style.scale = 2;
-
-        // Stop the animation
-        cancelAnimationFrame(animationFrameId);
-        // Update score
-        i += 1;
-        document.getElementById("score").innerText = i;
-        setTimeout(() => {
-            balloon.remove();
-        }, 500);
-    });
 }
 
-// Apply setup to all existing balloons
-document.querySelectorAll('#balloon').forEach(balloon => {
-    setupBalloon(balloon, 50, 50);
-});
+#space{
+    position: absolute;
+    width: 90%;
+    height: 90%;
+    border: 1px solid rebeccapurple;
+    top: 50%;
+    left: 50%;
+    translate: -50% -50%;
+}
 
-// Event listener to create new balloons on click
-const space = document.getElementById('space');
-space.addEventListener("click", (event) => {
-    const createBalloon = document.createElement("div");
-    createBalloon.id = "balloon";
-    createBalloon.style.position = "absolute"; // Ensure it's positioned correctly
-    document.body.appendChild(createBalloon);
-    
-    // Apply setup to the new balloon
-    setupBalloon(createBalloon, event.clientX, event.clientY);
-});
+#balloon {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    /* background-color: red; */
+    position: absolute;
+    top: 50px;
+    left: 50px;
+}
+
+#balloon::after {
+    content: '';
+    position: absolute;
+    top: 20px;
+    left: 50px;
+    translate: -50%;
+    width: 50%;
+    height: 50%;
+    background-image: var(--alpha-image);
+    background-size: var(--alpha-size);
+    opacity: .8;
+}
+
+#balloon::before{
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-image: url(assets/threade.png);
+    top: 58px;
+    left: 0px;
+}
+
+#score{
+    position: absolute;
+    right: 3%;
+    top: 3%;
+    font-family: "Comic Neue", cursive;
+    font-size: 5rem;
+    font-weight: bolder;
+    color: red;
+    -webkit-text-stroke: 2px black;
+    z-index: 1;
+}
+
+#cursor{
+    border-radius: 50%;
+    margin: 3px;
+    height: 15px;
+    width: 15px;
+
+    position: absolute;
+    border: 1px solid black;
+    transition: .0s linear;
+    z-index: 1;
+    transition: .2s ease;
+}
+
+#cursor::after{
+    content: "";
+    position: absolute;
+    width: 1px;
+    height: 35px;
+    left: 50%;
+    top: 50%;
+    translate: -50% -50%;
+    background-color: black;
+}
+#cursor::before{
+    content: "";
+    position: absolute;
+    width: 35px;
+    height: 1px;
+    left: 50%;
+    top: 50%;
+    translate: -50% -50%;
+    background-color: black;
+}
